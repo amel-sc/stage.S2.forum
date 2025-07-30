f1c04ed6d31fdabaaad92541f16139d5e7b1b0b3
CHARLES <amelsoanasycharles@gmail.com>
2025-07-30 14:49:27 +0300

    update code

--- BDD/forum.sql
+++ BDD/forum.sql
@@ -13,7 +13,7 @@ create table user (
     u_type int
 );
 
-create table subject (
+create table forum_subject (
     subject_id int auto_increment primary key,
     s_title text,
     s_content text,
@@ -30,6 +30,18 @@ create table comment (
     Constraint fk_user_comment foreign key (user_id) references
     user(user_id),
     Constraint fk_subject_comment foreign key (subject_id) references
-    subject(subject_id)
+    forum_subject(subject_id)
 );
 
+-- subject default values
+INSERT INTO subject (s_title, s_content, user_id) VALUES
+('Introduction à SQL', 'Les bases du langage SQL, SELECT, INSERT, UPDATE, DELETE.', 1),
+('Programmation en Python', 'Découverte de Python avec des exemples pratiques.', 1),
+('HTML et CSS', 'Créer une page web simple avec HTML5 et CSS3.', 1),
+('Algorithmes de tri', 'Explication des algorithmes comme le tri à bulles, rapide, etc.', 1),
+('Bases de données relationnelles', 'Comprendre les relations entre les tables et les clés étrangères.', 1);
+
+-- view subject / user
+create or replace view v_subject_user as 
+(select *, u.u_first_name, u.u_last_name, u.u_birth_date, u.u_gender, u.u_email, u.u_image from forum_subject s 
+join user u on s.user_id = u.id_user);
--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,18 @@
+<?php
+    // subject list
+    $subject = select_table("subject", null, null);
+?>
+
 <section class="d_container">
-    <h1>Success</h1>
+    <h1 class="text-center fw-bold">Forum</h1>
+    <?php foreach ($subject as $item) { ?>
+        <div class="card w-75 mb-3 m-auto">
+            <div class="card-body">
+                <h5 class="card-title"><?= $item['s_title'] ?></h5>
+                <p class="card-text"><?= $item['s_content'] ?></p>
+                <a href="#" class="btn btn-primary">Comment</a>
+            </div>
+        </div>
+    <?php } ?>
+
 </section>
